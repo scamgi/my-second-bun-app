b3bf9c7749acfd51d8485dfebae8ec7163230ce4
Giacomo Scampini <scampini.giacomo@gmail.com>
2025-10-31 15:23:43 +0100

    fix index.test.tsx

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -5,15 +5,14 @@ const domain = `http://${server.hostname}:${server.port}`;
 
 describe("Bun React App API", () => {
   it("should return HTML for the root route", async () => {
-    const req = new Request(`${domain}/`);
-    const res = await server.fetch(req);
+    // Note: We use the global fetch() here
+    const res = await fetch(`${domain}/`);
     expect(res.status).toBe(200);
     expect(res.headers.get("Content-Type")).toContain("text/html");
   });
 
   it("should handle GET requests to /api/hello", async () => {
-    const req = new Request(`${domain}/api/hello`);
-    const res = await server.fetch(req);
+    const res = await fetch(`${domain}/api/hello`);
     const data = await res.json();
 
     expect(res.status).toBe(200);
@@ -24,8 +23,7 @@ describe("Bun React App API", () => {
   });
 
   it("should handle PUT requests to /api/hello", async () => {
-    const req = new Request(`${domain}/api/hello`, { method: "PUT" });
-    const res = await server.fetch(req);
+    const res = await fetch(`${domain}/api/hello`, { method: "PUT" });
     const data = await res.json();
 
     expect(res.status).toBe(200);
@@ -37,8 +35,7 @@ describe("Bun React App API", () => {
 
   it("should handle dynamic routes like /api/hello/:name", async () => {
     const name = "Tester";
-    const req = new Request(`${domain}/api/hello/${name}`);
-    const res = await server.fetch(req);
+    const res = await fetch(`${domain}/api/hello/${name}`);
     const data = await res.json();
 
     expect(res.status).toBe(200);
